ca8e183f8fbc22b3cb9b83b0d1a52a9c29c11162
hakimfrh <hakimfrh145@gmail.com>
2023-11-01 22:03:10 +0700

    add comment

--- app/src/test/java/com/kkdev/junit/ExampleUnitTest.java
+++ app/src/test/java/com/kkdev/junit/ExampleUnitTest.java
@@ -7,8 +7,8 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void testAddition() {
-        MathOperation math = new MathOperation();
-        int result = math.add(3, 4);
-        assertEquals(7, result);
+        MathOperation math = new MathOperation(); //mencontruct MathOperation
+        int result = math.add(3, 4); //menggunkanan method math.add untuk menambah 3 dan 4
+        assertEquals(7, result); //apabila result yang dihasilkan sama dengan expected result yaitu 7. maka hasil akan pass
     }
 }
